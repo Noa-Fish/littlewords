588f3f1e957715b5198a1435e5e740dcbf4823d9
Noa-Fish <95444542+Noa-Fish@users.noreply.github.com>
2023-03-01 14:28:44 +0100

    add login button

--- lib/routes/login/login_route.dart
+++ lib/routes/login/login_route.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart' ;
 import 'package:littlewords/widgets/littlewords_logo.dart';
 
 class LoginRoute extends StatelessWidget {
-  const LoginRoute({Key? key}) : super(key: key);
+  LoginRoute({Key? key}) : super(key: key);
+
+  final _txtController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
-        children  : const [
+        children  :  [
           Spacer(),
 
           LittlewordsLogo(),
@@ -17,17 +19,30 @@ class LoginRoute extends StatelessWidget {
           Spacer(),
 
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8),
+            padding: EdgeInsets.symmetric(horizontal: 8 , vertical: 16),
             child: TextField(
+              controller: _txtController,
               decoration: InputDecoration(
                   fillColor: Colors.red,
                   filled: true),
               ),
           ),
-          ElevatedButton(
-              onPressed: null,
-              child: Text('Enregiistrer nom'))
+          Padding(
+            padding: EdgeInsets.only(top: 8.0,left: 8.0,right: 8.0),
+            child: ElevatedButton(
+                onPressed: _onPressed,
+                style: ElevatedButton.styleFrom(
+                  minimumSize:Size.fromHeight(48)
+                ),
+                child :Text('Enregistrer nom')
+            ),
+          )
         ]),
     );
+  }
+
+  void _onPressed() {
+    var text = _txtController.text;
+    print('Text: $text');
   }
 }
