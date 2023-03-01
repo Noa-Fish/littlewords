1fa3eb2e773f79971fb83cfe961d427410722fd8
Noa-Fish <95444542+Noa-Fish@users.noreply.github.com>
2023-03-01 14:07:23 +0100

    add Logo and start login page

--- lib/routes/login/login_route.dart
+++ lib/routes/login/login_route.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart' ;
+import 'package:littlewords/widgets/littlewords_logo.dart';
 
 class LoginRoute extends StatelessWidget {
   const LoginRoute({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children  : const [
+          Spacer(),
+
+          LittlewordsLogo(),
+
+          Spacer(),
+
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: TextField(
+              decoration: InputDecoration(
+                  fillColor: Colors.red,
+                  filled: true),
+              ),
+          ),
+          ElevatedButton(
+              onPressed: null,
+              child: Text('Enregiistrer nom'))
+        ]),
+    );
   }
 }
---
+++ lib/widgets/littlewords_logo.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LittlewordsLogo extends StatelessWidget {
+  const LittlewordsLogo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 128,
+      height: 128,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.blue,
+        border: Border.all(color: Colors.black,width:5)
+      ),
+    );
+  }
+}
