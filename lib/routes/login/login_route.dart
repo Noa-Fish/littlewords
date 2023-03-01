b58da773c022395fca509e51a8433cfa6be00549
Noa-Fish <95444542+Noa-Fish@users.noreply.github.com>
2023-03-01 15:02:17 +0100

    add connexion with login

--- lib/routes/login/login_route.dart
+++ lib/routes/login/login_route.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart' ;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:littlewords/widgets/littlewords_logo.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../providers/username.provider.dart';
 
 class LoginRoute extends StatelessWidget {
   LoginRoute({Key? key}) : super(key: key);
 
-  final _txtController = TextEditingController();
+  final TextEditingController _txtController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -29,20 +33,38 @@ class LoginRoute extends StatelessWidget {
           ),
           Padding(
             padding: EdgeInsets.only(top: 8.0,left: 8.0,right: 8.0),
-            child: ElevatedButton(
-                onPressed: _onPressed,
-                style: ElevatedButton.styleFrom(
-                  minimumSize:Size.fromHeight(48)
-                ),
-                child :Text('Enregistrer nom')
+            child :SaveUsernameButton(
+              controller: _txtController,
             ),
           )
         ]),
     );
   }
+}
 
-  void _onPressed() {
-    var text = _txtController.text;
+class SaveUsernameButton extends ConsumerWidget {
+  const SaveUsernameButton({Key? key,required this.controller }) : super(key: key);
+
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ElevatedButton(
+        onPressed: () => _onPressed(ref),
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size.fromHeight(48),
+    ),
+        child: const Text('Enregistrer Nom'),
+    );
+  }
+
+  void _onPressed(WidgetRef ref){
+    var text = controller.text;
     print('Text: $text');
+
+    SharedPreferences.getInstance().then((prefs) {
+      prefs.setString('username', text);
+      ref.refresh(usernameProvider);
+    });
   }
 }
