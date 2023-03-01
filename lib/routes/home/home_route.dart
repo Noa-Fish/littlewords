5a72ee8eef8bf694072d895a309d2299d00995be
Noa-Fish <95444542+Noa-Fish@users.noreply.github.com>
2023-03-01 16:11:04 +0100

    add Page 1 & 2 and floatingActionbutton for page 3

--- lib/routes/home/home_route.dart
+++ lib/routes/home/home_route.dart
@@ -1,10 +1,94 @@
 import 'package:flutter/material.dart' ;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeRoute extends StatelessWidget {
+class HomeRoute extends StatefulWidget {
   const HomeRoute({Key? key}) : super(key: key);
 
   @override
+  State<HomeRoute> createState() => _HomeRouteState();
+
+}
+
+class _HomeRouteState extends State<HomeRoute> {
+
+  int bottomNavigationIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    final bodies = <Widget>[
+      _PageA(),
+      _PageB(),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Details'),
+      ),
+      body: bodies[bottomNavigationIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: bottomNavigationIndex,
+        onTap: (int index) {
+          setState(() {
+            bottomNavigationIndex = index;
+          });
+        },
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'A',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dangerous),
+            label: 'B',
+          ),
+        ],
+      ),
+
+        floatingActionButton: bottomNavigationIndex == 0 ? Consumer(
+          builder: (context,ref,child){
+            return FloatingActionButton(
+              onPressed: () {
+              },
+              child: const Icon(Icons.arrow_back),
+            );
+          },
+        ) : null,
+
+    );
   }
 }
+
+
+
+class _PageA extends StatelessWidget {
+  _PageA({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Page A"),
+
+      ),
+    );
+  }
+}
+
+
+
+class _PageB extends StatelessWidget {
+  _PageB({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Page B"),
+      ),
+    );
+  }
+}
+
